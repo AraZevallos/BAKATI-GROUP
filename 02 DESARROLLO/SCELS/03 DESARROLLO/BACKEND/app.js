c4342bea7239ed44366068800041803b11315678
Guillermo Savero <guillermo.savero@unmsm.edu.pe>
2022-06-15 19:01:54 -0500

    feat(backend): Agrega rutas y modelos

--- 02 DESARROLLO/SCELS/03 DESARROLLO/BACKEND/app.js
+++ 02 DESARROLLO/SCELS/03 DESARROLLO/BACKEND/app.js
@@ -2,20 +2,34 @@ const express = require("express");
 const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 require("dotenv/config");
 
-const api = process.env.API_URL;
+app.use(cors());
+app.options('*', cors())
+
 
 //Middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 
-//http://localhost:3000/api/v1/products
-app.get("/", (req, res) => {
-  res.send("hello API!");
-});
 
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+
+
+//Database
 mongoose.connect(process.env.CONNECTION_STRING,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -28,6 +42,8 @@ mongoose.connect(process.env.CONNECTION_STRING,{
   console.log(err);
 })
 
+
+//Server
 app.listen(3000, () => {
   console.log(api);
   console.log("server is running http://localhost:3000");
