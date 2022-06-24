6250798ffe4a5ab136c1fc9ba5c675b87b55fc69
frank jacobo <frank.jacobo@unmsm.edu.pe>
2022-06-23 23:10:15 -0500

    feat(backend): Agrega middleware de producto

--- 02 DESARROLLO/SCELS/03 DESARROLLO/BACKEND/middlewares/validate_models.js
+++ 02 DESARROLLO/SCELS/03 DESARROLLO/BACKEND/middlewares/validate_models.js
@@ -11,4 +11,24 @@ function validate_category(req, res, next) {
     next()
 }
 
-module.exports = { validate_category }
+function validate_product(req, res, next) {
+    const { error } = Joi.object({
+        name: Joi.string().required(),
+        description: Joi.string().required(),
+        richDescription: Joi.string().default(''),
+        image: Joi.string().default(''),
+        images: Joi.array().default([]),
+        brand: Joi.string().default(''),
+        price: Joi.number().default(0),
+        category: Joi.string().required(),
+        countInStock: Joi.number().required().min(0).max(255),
+        rating: Joi.number().default(0),
+        numReviews: Joi.number().default(0),
+        isFeatured: Joi.boolean().default(false),
+        dateCreated: Joi.date().default(Date.now),
+    })
+    if (error) { next(createError(400, error)) }
+    next()
+}
+
+module.exports = { validate_category, validate_product }
