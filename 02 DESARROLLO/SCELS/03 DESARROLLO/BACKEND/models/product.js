3dc867ec5670a8098023e1d6df637eaeed7dcab9
frank jacobo <frank.jacobo@unmsm.edu.pe>
2022-06-23 22:59:38 -0500

    feat(backend): Agrega modelo de producto

--- 02 DESARROLLO/SCELS/03 DESARROLLO/BACKEND/models/product.js
+++ 02 DESARROLLO/SCELS/03 DESARROLLO/BACKEND/models/product.js
@@ -1,11 +1,44 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
+    name: {
+        type: String, required: true
+    },
+    description: {
+        type: String, required: true
+    },
+    richDescription: {
+        type: String, default: ''
+    },
+    image: {
+        type: String, default: ''
+    },
+    images: [{
+        type: String, default: ''
+    }],
+    brand: {
+        type: String, default: ''
+    },
+    price: {
+        type: Number, default: 0
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true
+    },
     countInStock: {
-        type: Number,
-        required: true
+        type: Number, required: true, min: 0, max: 255
+    },
+    rating: {
+        type: Number, default: 0
+    },
+    numReviews: {
+        type: Number, default: 0
+    },
+    isFeatured: {
+        type: Boolean, default: false
+    },
+    dateCreated: {
+        type: Date, default: Date.now
     }
 })
 
