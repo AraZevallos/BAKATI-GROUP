fd79ba0c03fed53aa5c4828dfc44491259328a23
Guillermo Savero <guillermo.savero@unmsm.edu.pe>
2022-07-06 12:49:53 -0500

    feat(frontend): Agrega inicio de sistema administrador

--- 02 DESARROLLO/SCELS/03 DESARROLLO/FRONTEND/libs/products/src/lib/services/categories.service.ts
+++ 02 DESARROLLO/SCELS/03 DESARROLLO/FRONTEND/libs/products/src/lib/services/categories.service.ts
@@ -16,4 +16,12 @@ export class CategoriesService {
    getCategories(): Observable<Category[]>{
     return this.http.get<Category[]>('http://localhost:3000/api/v1/categories/')
    }
+
+   createCategory(category: Category): Observable<Category>{
+      return this.http.post<Category>('http://localhost:3000/api/v1/categories/', category)
+   }
+
+   deleteCategory(categoryId: string): Observable<Object>{
+    return this.http.delete<Object>(`http://localhost:3000/api/v1/categories/${categoryId}`)
+ }
 }
