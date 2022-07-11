ee068fe241232bbd476ad99d899bb11acef037e8
Araccelli Zevallos <araccelli.zevallos@gmail.com>
2022-07-11 01:00:02 -0500

    feat(backend): agrega middleware en modulo Ordenes

---
+++ 02 DESARROLLO/SCELS/03 DESARROLLO/BACKEND/middlewares/validate-order.js
@@ -0,0 +1,25 @@
+const createError = require('http-errors')
+const Joi = require('joi')
+Joi.objectId = require('joi-objectid')(Joi)
+
+function validateOrder(req, res, next) {
+  const { error } = Joi.object({
+    orderItems: Joi.array().required({
+      product: Joi.objectId().required(),
+      quantity: Joi.number().required()
+    }),
+    shippingAddress1: Joi.string().required(),
+    shippingAddress2: Joi.string(),
+    city: Joi.string().required(),
+    zip: Joi.string().required(),
+    country: Joi.string().required(),
+    phone: Joi.string().required(),
+    status: Joi.string().required(),
+    totalPrice: Joi.number().required(),
+    user: Joi.ObjectId().required(),
+    dateOrdered: Joi.date().default(Date.now)
+  }).validate(req.body)
+  if (error) { return next(createError(400, error)) }
+  next()
+}
+module.exports = { validateOrder }
