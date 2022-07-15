6546c7ccb0d535321ac331c052ff9e9e2829f60a
Guillermo Savero <guillermo.savero@unmsm.edu.pe>
2022-07-15 12:32:20 -0500

    feat(frontend): Agrega carrito de compras inicial

--- 02 DESARROLLO/SCELS/03 DESARROLLO/FRONTEND/libs/orders/src/lib/orders.module.ts
+++ 02 DESARROLLO/SCELS/03 DESARROLLO/FRONTEND/libs/orders/src/lib/orders.module.ts
@@ -1,10 +1,52 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Route } from '@angular/router';
+import { RouterModule, Route, Routes } from '@angular/router';
+import { CartService } from './services/cart.service';
+import { CartIconComponent } from './components/cart-icon/cart-icon.component';
+import {BadgeModule} from 'primeng/badge';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
+import { ButtonModule } from 'primeng/button';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { OrderSummaryComponent } from './components/order-summary/order-summary.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CheckoutPageComponent } from './pages/checkout-page/checkout-page.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputMaskModule } from 'primeng/inputmask';
+import { DropdownModule } from 'primeng/dropdown';
+
 
 export const ordersRoutes: Route[] = [];
 
+const routes: Routes = [
+  {
+    path: 'cart',
+    component: CartPageComponent
+  },
+  {
+    path: 'checkout',
+    component: CheckoutPageComponent
+  }
+]
+
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule.forChild(routes), BadgeModule, ButtonModule, 
+            InputNumberModule, FormsModule, ReactiveFormsModule ,InputTextModule, InputMaskModule,DropdownModule],
+  declarations: [
+    CartIconComponent,
+    CartPageComponent,
+    OrderSummaryComponent,
+    CheckoutPageComponent
+  ],
+  exports: [
+    CartIconComponent,
+    CartPageComponent,
+    OrderSummaryComponent,
+    CheckoutPageComponent
+  ],
 })
-export class OrdersModule {}
+export class OrdersModule {
+  constructor(cartService: CartService) {
+
+    cartService.initCartLocalStorage();
+  }
+}
