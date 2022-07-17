e04c46497b6cd96d0d9420425676e78fc6cd7a24
frank jacobo <frank.jacobo@unmsm.edu.pe>
2022-07-16 19:05:38 -0500

    fix(backend): Corrige middleware del modulo productos

--- 02 DESARROLLO/SCELS/03 DESARROLLO/BACKEND/middlewares/validate-jwt.js
+++ 02 DESARROLLO/SCELS/03 DESARROLLO/BACKEND/middlewares/validate-jwt.js
@@ -2,31 +2,21 @@ const { expressjwt: jwt } = require('express-jwt')
 
 function authenticate() {
   let api = process.env.API_URL
-  return jwt({
-    secret: process.env.JWT_KEY,
-    algorithms: ['HS256'],
-    isRevoked: isRevoked
-  })
+  return jwt({ secret: process.env.JWT_KEY, algorithms: ['HS256'], isRevoked: isRevoked })
     .unless({
       path: [
+        { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
         { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
         { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
         `${api}/users/login`,
         `${api}/users/register`,
-        //{url: /(.*)/},
       ]
     })
 }
-// async function isRevoked(req, payload, done) {
-//   if (!payload.isAdmin) {
-//     return
-//   }
-//   return payload='null'
-// }
-async function isRevoked(req, token){ 
-  if(!token.payload.isAdmin) { 
-    return true; 
-  } 
+async function isRevoked(req, token) {
+  if (!token.payload.isAdmin) {
+    return true;
+  }
 }
 
 module.exports = authenticate
