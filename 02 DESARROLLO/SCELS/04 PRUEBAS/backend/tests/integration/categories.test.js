4be4e2a11b1e289380c21b3ee0b73f433ebfd5ea
frank jacobo <frank.jacobo@unmsm.edu.pe>
2022-07-21 23:29:24 -0500

    test(backend): Agrega prueba del metodo POST del modulo Categorias

--- 02 DESARROLLO/SCELS/04 PRUEBAS/backend/tests/integration/categories.test.js
+++ 02 DESARROLLO/SCELS/04 PRUEBAS/backend/tests/integration/categories.test.js
@@ -43,4 +43,49 @@ describe('Categories', () => {
       expect(res.body).toHaveProperty('name', name)
     })
   })
+
+  describe('POST /categories', () => {
+
+    let token, name
+    const exec = async () => {
+      return await request(server).post('/api/v1/categories')
+        .set('Authorization', 'bearer ' + token).send({ name })
+    }
+
+    beforeEach(() => {
+      token = new User({ _id: mongoose.Types.ObjectId(), isAdmin: true })
+        .generateAuthToken()
+      name = 'Category 1'
+    })
+
+    it('should return a 401 if client is not logged in', async () => {
+      token = ' '
+      const res = await exec()
+      expect(res.status).toBe(401)
+    })
+
+    it('should return a 201 if client is logged in and admin', async () => {
+      const res = await exec()
+      expect(res.status).toBe(201)
+    })
+
+    it('should return a 400 if category is invalid', async () => {
+      name = ''
+      const res = await exec()
+      expect(res.status).toBe(400)
+    })
+
+    it('should save the category if it is valid', async () => {
+      await exec()
+      const category = await Category.find({ name })
+      expect(category).not.toBeNull()
+    })
+
+    it('should return the category if it is valid', async () => {
+      const res = await exec()
+      expect(res.body).toHaveProperty('_id')
+      expect(res.body).toHaveProperty('name', name)
+    })
+
+  })
 })
