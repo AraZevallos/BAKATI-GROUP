05df152814843c3bbe0a4dd1bc99b2316cf828aa
frank jacobo <frank.jacobo@unmsm.edu.pe>
2022-07-22 02:32:53 -0500

    test(backend): Agrega prueba del metodo POST del modulo Ordenes

--- 02 DESARROLLO/SCELS/04 PRUEBAS/backend/middlewares/validate-jwt.js
+++ 02 DESARROLLO/SCELS/04 PRUEBAS/backend/middlewares/validate-jwt.js
@@ -8,7 +8,6 @@ function authenticate() {
         { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
         { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
         { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
-        { url: /\/api\/v1\/orders(.*)/, methods: ['POST', 'OPTIONS'] },
         `${api}/users/login`,
         `${api}/users/register`,
       ]
--- 02 DESARROLLO/SCELS/04 PRUEBAS/backend/tests/integration/orders.test.js
+++ 02 DESARROLLO/SCELS/04 PRUEBAS/backend/tests/integration/orders.test.js
@@ -1,8 +1,7 @@
 const request = require('supertest')
 const mongoose = require('mongoose')
-const { Category } = require('../../models/category')
-const { Product } = require('../../models/product')
 const { User } = require('../../models/user')
+const { Product } = require('../../models/product')
 const { OrderItem } = require('../../models/order-item')
 const { Order } = require('../../models/order')
 
@@ -10,7 +9,7 @@ let server
 describe('Orders', () => {
 
   beforeEach(() => { server = require('../../bin/www') })
-  afterEach(async () => { await server.close(); await Order.deleteMany({}); await User.deleteMany({}) })
+  afterEach(async () => { await server.close(); await Order.deleteMany({}); await User.deleteMany({}); await Product.deleteMany({}); await OrderItem.deleteMany({}) })
 
   describe('GET /orders', () => {
 
@@ -37,7 +36,7 @@ describe('Orders', () => {
     })
 
     it('should return a 401 if client is not logged in', async () => {
-      token = ''
+      token = ' '
       const res = await exec()
       expect(res.status).toBe(401)
     })
@@ -76,7 +75,7 @@ describe('Orders', () => {
     })
 
     it('should return a 401 if client is not logged in', async () => {
-      token = ''
+      token = ' '
       const res = await exec()
       expect(res.status).toBe(401)
     })
@@ -107,4 +106,62 @@ describe('Orders', () => {
 
   })
 
+  describe('POST /orders', () => {
+
+    let token, user, order
+    const exec = async () => {
+      return await request(server).post('/api/v1/orders')
+        .set('Authorization', 'bearer ' + token).send(order)
+    }
+
+    beforeEach(async () => {
+      user = new User({ name: 'access', email: 'access', password: 'access', phone: '982987654', isAdmin: true })
+      token = user.generateAuthToken()
+      let tempProduct1 = new Product({ name: 'producto1', price: 100, description: 'descripcion1', category: mongoose.Types.ObjectId().toHexString(), countInStock: 10 })
+      let tempProduct2 = new Product({ name: 'producto2', price: 200, description: 'descripcion2', category: mongoose.Types.ObjectId().toHexString(), countInStock: 20 })
+      await user.save()
+      await tempProduct1.save()
+      await tempProduct2.save()
+      orderItems = [{ product: tempProduct1._id.toHexString(), quantity: 1 }, { product: tempProduct2._id.toHexString(), quantity: 2 }]
+      order = {
+        user: user._id.toHexString(), orderItems: orderItems, status: 'entregado',
+        shippingAddress1: 'calle 1', city: 'ciudad1', zip: 'zip1', country: 'pais1', phone: 'telefono1'
+      }
+    })
+
+    it('should return a 401 if client is not logged in', async () => {
+      token = ' '
+      const res = await exec()
+      expect(res.status).toBe(401)
+    })
+
+    it('should return a 201 if client is logged in and admin', async () => {
+      const res = await exec()
+      expect(res.status).toBe(201)
+    })
+
+    it('should return a 400 if order is invalid', async () => {
+      order = { orderItems: orderItems }
+      const res = await exec()
+      expect(res.status).toBe(400)
+    })
+
+    it('should save the order if it is valid', async () => {
+      const res = await exec()
+      const orderInDb = await Order.findById(res.body._id)
+      expect(orderInDb).not.toBeNull()
+    })
+
+    it('should return the order if it is valid', async () => {
+      const res = await exec()
+      expect(res.body).toHaveProperty('user')
+      expect(res.body).toHaveProperty('status', order.status)
+      expect(res.body).toHaveProperty('totalPrice')
+      expect(res.body).toHaveProperty('shippingAddress1', order.shippingAddress1)
+      expect(res.body).toHaveProperty('city', order.city)
+      expect(res.body).toHaveProperty('zip', order.zip)
+      expect(res.body).toHaveProperty('country', order.country)
+      expect(res.body).toHaveProperty('phone', order.phone)
+    })
+  })
 })
