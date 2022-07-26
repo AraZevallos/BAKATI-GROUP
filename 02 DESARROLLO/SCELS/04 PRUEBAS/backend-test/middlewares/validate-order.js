dd3c9637d8a8e481068a91440172c4dbe2bc2146
Araccelli Zevallos <araccelli.zevallos@gmail.com>
2022-07-25 23:36:47 -0500

    test(backend): agrega prueba unitaria de validador de ordenes

--- 02 DESARROLLO/SCELS/04 PRUEBAS/backend-test/middlewares/validate-order.js
+++ 02 DESARROLLO/SCELS/04 PRUEBAS/backend-test/middlewares/validate-order.js
@@ -1,10 +1,13 @@
-const createError = require('http-errors')
-const Joi = require('joi')
-Joi.objectId = require('joi-objectid')(Joi)
+const createError = require("http-errors");
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
 
 function validateOrder(req, res, next) {
   const { error } = Joi.object({
-    orderItems: Joi.array().items({ product: Joi.objectId().required(), quantity: Joi.number().required() }),
+    orderItems: Joi.array().items({
+      product: Joi.objectId().required(),
+      quantity: Joi.number().required(),
+    }),
     shippingAddress1: Joi.string().required(),
     shippingAddress2: Joi.string(),
     city: Joi.string().required(),
@@ -14,28 +17,38 @@ function validateOrder(req, res, next) {
     status: Joi.string().required(),
     totalPrice: Joi.number(),
     user: Joi.objectId().required(),
-    dateOrdered: Joi.date().default(Date.now)
-  }).validate(req.body)
-  if (error) { return next(createError(400, error)) }
-  next()
+    dateOrdered: Joi.date().default(Date.now),
+  }).validate(req.body);
+  if (error) {
+    return next(createError(400, error));
+  }
+  next();
 }
 
 function validateOrderStatus(req, res, next) {
-  const status = ['pendiente', 'en proceso', 'entregado']
+  const status = ["pendiente", "en proceso", "entregado"];
   const { error } = Joi.object({
-    status: Joi.string().valid(...Object.values(status)).required()
-  }).validate(req.body)
-  if (error) { return next(createError(400, error)) }
-  next()
+    status: Joi.string()
+      .valid(...Object.values(status))
+      .required(),
+  }).validate(req.body);
+  if (error) {
+    return next(createError(400, error));
+  }
+  next();
 }
 
 function validateOrderCheckout(req, res, next) {
-  const { error } = Joi.array().items({
-    product: Joi.objectId().required(),
-    quantity: Joi.number().required()
-  }).validate(req.body)
-  if (error) { return next(createError(400, error)) }
-  next()
+  const { error } = Joi.array()
+    .items({
+      product: Joi.objectId().required(),
+      quantity: Joi.number().required(),
+    })
+    .validate(req.body);
+  if (error) {
+    return next(createError(400, error));
+  }
+  next();
 }
 
-module.exports = { validateOrder, validateOrderStatus, validateOrderCheckout }
+module.exports = { validateOrder, validateOrderStatus, validateOrderCheckout };
