159108d3bfa4b938bfb20fe503cbdd23b0d644ba
SEBASTIAN GABRIEL TORRES DOMENACK <sebastian.torres@unmsm.edu.pe>
2023-04-23 17:05:04 -0500

    Agrega postman y arregla endpoint usuario

--- 02 DESARROLLO/SCELS/03 DESARROLLO/BACKEND/routes/users.js
+++ 02 DESARROLLO/SCELS/03 DESARROLLO/BACKEND/routes/users.js
@@ -1,17 +1,26 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const { validateId } = require('../middlewares/validate-id')
-const { validateAuth, validateUser } = require('../middlewares/validate-user')
-const { getAllUsers, getUserById, createUser } = require('../controllers/controller-users')
-const { updateUser, deleteUser, loginUser, getCountUsers } = require('../controllers/controller-users')
+const { validateId } = require('../middlewares/validate-id');
+const { validateAuth, validateUser } = require('../middlewares/validate-user');
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+} = require('../controllers/controller-users');
+const {
+  updateUser,
+  deleteUser,
+  loginUser,
+  getCountUsers,
+} = require('../controllers/controller-users');
 
-router.get('/', [getAllUsers])
-router.get('/:id', [validateId, getUserById])
-router.post('/', [validateUser, createUser])
-router.put('/:id', [validateId, validateUser, updateUser])
-router.delete('/:id', [validateId, deleteUser])
-router.post('/login', [validateAuth, loginUser])
-router.get('/get/count', [getCountUsers])
+router.get('/', [getAllUsers]);
+router.get('/:id', [validateId, getUserById]);
+router.post('/', [validateUser, createUser]);
+router.put('/:id', [validateId, validateUser, updateUser]);
+router.delete('/:id', [validateId, deleteUser]);
+router.post('/login', [validateAuth, loginUser]);
+router.get('/get/count', [getCountUsers]);
 
-module.exports = router
+module.exports = router;
